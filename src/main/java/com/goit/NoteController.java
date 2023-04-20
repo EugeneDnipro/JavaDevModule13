d43c1ba66971df69661f13003bc29d4c102b80a3
EugeneDnipro <EugeneDnepr@gmail.com>
2023-04-20 15:38:36 +0300

    Seeking delete problems

--- src/main/java/com/goit/NoteController.java
+++ src/main/java/com/goit/NoteController.java
@@ -1,12 +1,13 @@
 package com.goit;
 
 import com.goit.CrudServices.NoteService;
+import com.goit.Entities.Note;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import static com.goit.JavaDevModule13Application.notesStorage;
 
 @Controller
 @RequestMapping("/note")
@@ -22,8 +23,18 @@ public class NoteController {
 
     @PostMapping("/delete")
     public String noteDelete(@RequestParam(name = "id") long id) {
-        ModelAndView result = new ModelAndView("note/delete");
+        //ModelAndView result = new ModelAndView("note/delete");
+        System.out.println("id = " + id);
         noteService.deleteById(id);
-        return "redirect:/note/list";
+        return "redirect:/note/delete";
+        //return "/note/delete";
     }
+
+//    @PostMapping("/delete")
+//    public String deleteNote(@RequestParam("id") long id, Model model) {
+//        Note note = notesStorage.get(id);
+//        model.addAttribute("note", note);
+//        return "delete";
+//    }
+
 }
